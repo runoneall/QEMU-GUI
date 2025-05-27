47cca8d11340e392cb361f8617e252f3ba30400e
runoneall <runoobsteve@hotmail.com>
2025-05-27 10:05:03 +0800

    update

--- main.go
+++ main.go
@@ -1,1 +1,20 @@
 package main
+
+import (
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/widget"
+)
+
+func main() {
+	fyne := app.New()
+
+	// windows
+	w_main := fyne.NewWindow("QEMU Generator")
+
+	// main window layout
+	w_main.Resize(fyne.New(150, 300))
+	w_main.SetContent(widget.NewLabel("Hello, Fyne!"))
+
+	// show window
+	w_main.ShowAndRun()
+}
