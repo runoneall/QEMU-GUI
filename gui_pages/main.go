8597b57e1250d7372127c09857c7a8773ecb80eb
runoneall <runoobsteve@hotmail.com>
2025-05-30 23:25:03 +0800

    update

--- gui_pages/main.go
+++ gui_pages/main.go
@@ -3,6 +3,7 @@ package gui_pages
 import (
 	"context"
 	"image/color"
+	"os"
 	"qemu-gui/helper"
 	"qemu-gui/qemu_manager"
 	"strings"
@@ -44,8 +45,10 @@ func Main_Page(myApp fyne.App) *fyne.Container {
 
 			// start vm
 			widget.NewButtonWithIcon("Start", theme.MediaPlayIcon(), func() {
-				t1, _ := vmConfig.BuildOption()
-				t1.Start(context.Background())
+				qemu_target, _ := vmConfig.BuildOption()
+				qemu_target.SerialOutput = append(qemu_target.SerialOutput, os.Stdout)
+				vm, _ := qemu_target.Start(context.Background())
+				vm.Wait()
 			}),
 
 			// stop vm
